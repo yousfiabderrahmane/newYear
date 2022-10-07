7aa5e994727e58db818f9d2b7f27ceae0573893d
Yousfi Abderrahmane <bdrrhmnyousfi@gmail.com>
2022-10-07 10:08:13 +0100

    creating the countdown

---
+++ main.js
@@ -0,0 +1,24 @@
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+const countdown = document.getElementById("countdown");
+
+const currentyear = new Date().getFullYear();
+
+const newYearTime = new Date(`January 01 ${currentyear + 1} 00:00:00`);
+
+function updateCountdown() {
+  const currentTime = new Date();
+  const diff = newYearTime - currentTime;
+  const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+  const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+  const m = Math.floor(diff / 1000 / 60) % 60;
+  const s = Math.floor(diff / 1000) % 60;
+  days.innerText = d;
+  hours.innerText = h < 10 ? "0" + h : h;
+  minutes.innerText = m < 10 ? "0" + m : m;
+  seconds.innerText = s < 10 ? "0" + s : s;
+}
+
+setInterval(updateCountdown, 1000);
